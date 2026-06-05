7ad7dfab83e54e6790b0ca54d033b18a2c69228b
henok3878 <81500872+henok3878@users.noreply.github.com>
2026-06-05 11:32:43 -0400

    codex-meter 0.1.1

--- Formula/codex-meter.rb
+++ Formula/codex-meter.rb
@@ -1,15 +1,15 @@
 class CodexMeter < Formula
   desc "Lightweight terminal dashboard for Codex usage"
   homepage "https://github.com/h3nock/codex-meter"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/h3nock/codex-meter/releases/download/v0.1.0/codex-meter-v0.1.0-macos-aarch64.tar.gz"
-    sha256 "ddfdb45a7ddee238fc710f02bb4a32b5941c33df103d4e7cb042242fdc247823"
+    url "https://github.com/h3nock/codex-meter/releases/download/v0.1.1/codex-meter-v0.1.1-macos-aarch64.tar.gz"
+    sha256 "cbd64f13eafc13f32f5d98706c8dd9933a320161868210d21fe202e6136f322f"
   else
-    url "https://github.com/h3nock/codex-meter/releases/download/v0.1.0/codex-meter-v0.1.0-macos-x86_64.tar.gz"
-    sha256 "1b2f1859926167ae8f7475c6d8e80afa1da83c2c2ccceabc908a0239798e3b2f"
+    url "https://github.com/h3nock/codex-meter/releases/download/v0.1.1/codex-meter-v0.1.1-macos-x86_64.tar.gz"
+    sha256 "140755dc871323bd8c396b7d65b7bb1b7872dfb40074078f95c1ece119c4e3a1"
   end
 
   depends_on :macos
